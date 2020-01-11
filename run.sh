3468bc4b00766e2b5ebeadcc7c5233c99dfd3f6b
Jason Eric <eritpchy@qq.com>
2020-01-11 11:06:26 +0800

    + 3.9.6 适配 Magisk 20.2

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-./gradlew clean && ./gradlew :riru-module-xfingerprint-pay-taobao:zip && adb push ./release/* /sdcard/Download/
+#!/bin/bash
+set -e
+cd ${0%/*}
+./gradlew clean
+./gradlew :module:assembleRelease
+adb shell rm -fv "/data/local/tmp/libxfingerprint_pay_taobao.dex"
+adb push ./out/*.zip /sdcard/Download/
