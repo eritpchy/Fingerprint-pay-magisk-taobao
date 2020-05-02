9ac366ce6a9da274b0d334bff786cd00b1396f31
Jason Eric <eritpchy@qq.com>
2020-05-02 17:57:21 +0800

    + 3.9.10 适配 Magisk 20.4

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -e
 cd ${0%/*}
 ./gradlew clean
 ./gradlew :module:assembleRelease
-adb shell rm -fv "/data/local/tmp/libxfingerprint_pay_taobao.dex"
+adb shell rm -fv "/data/local/tmp/libriru_module_xfingerprint_pay_taobao.dex"
 adb push ./out/*.zip /sdcard/Download/
